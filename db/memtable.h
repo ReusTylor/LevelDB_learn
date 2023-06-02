ba332480258677b523513549c1c190bb80db5d5c
ReusTylor <554105237@qq.com>
2023-06-02 11:46:50 +0800

    memtable.h

--- db/memtable.h
+++ db/memtable.h
@@ -1,7 +1,7 @@
 // Copyright (c) 2011 The LevelDB Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file. See the AUTHORS file for names of contributors.
-
+// memtable类代表内存表，用于存储数据和操作内存中的键值对
 #ifndef STORAGE_LEVELDB_DB_MEMTABLE_H_
 #define STORAGE_LEVELDB_DB_MEMTABLE_H_
 
@@ -27,9 +27,11 @@ class MemTable {
   MemTable& operator=(const MemTable&) = delete;
 
   // Increase reference count.
+  //ref()，unref()增加和减少计数，用于管理memtable的生命周期
   void Ref() { ++refs_; }
 
   // Drop reference count.  Delete if no more references exist.
+  // 在引用计数减少到0的时候删除memtable对象
   void Unref() {
     --refs_;
     assert(refs_ >= 0);
@@ -40,6 +42,7 @@ class MemTable {
 
   // Returns an estimate of the number of bytes of data in use by this
   // data structure. It is safe to call when MemTable is being modified.
+  // 返回估计的内存使用量，用于获取当前数据结构占用的内存大小。
   size_t ApproximateMemoryUsage();
 
   // Return an iterator that yields the contents of the memtable.
@@ -48,11 +51,13 @@ class MemTable {
   // while the returned iterator is live.  The keys returned by this
   // iterator are internal keys encoded by AppendInternalKey in the
   // db/format.{h,cc} module.
+  // 创建一个迭代器，用于遍历memtable中的内容
   Iterator* NewIterator();
 
   // Add an entry into memtable that maps key to value at the
   // specified sequence number and with the specified type.
   // Typically value will be empty if type==kTypeDeletion.
+  // 将键值对添加到MemTable中，指定序列号，值类型，键和值
   void Add(SequenceNumber seq, ValueType type, const Slice& key,
            const Slice& value);
 
@@ -60,6 +65,7 @@ class MemTable {
   // If memtable contains a deletion for key, store a NotFound() error
   // in *status and return true.
   // Else, return false.
+  // 查找对应的值
   bool Get(const LookupKey& key, std::string* value, Status* s);
 
  private:
